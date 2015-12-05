28ba5fc2e2afe3451112f2e6dba7c17e47a81ccf
Dillon Huff <dillonhuff@gmail.com>
2015-12-05 15:48:23 -0800

    Cleaned up Lift.hs

--- src/Lift.hs
+++ src/Lift.hs
@@ -23,22 +23,26 @@ liftUnary t l =
   case t == l of
    True -> Just ([], t)
    False ->
-     case isAp t && isAp l && (callHead t) == (callHead l) of
-      True ->
-        let h = callHead t
-            ta = callArgs t
-            la = callArgs l
-            res = sequence $ L.zipWith liftUnary ta la in
-         case res of
-          Just a ->
-            let calls = L.concatMap fst a
-                args = L.map snd a in
-             Just (calls, ap h args)
-          Nothing -> Nothing
-      False ->
-        case isAp l && (length $ callArgs l) == 1 && ((callArgs l) !! 0) == t
-             && (tToT $ gblType $ callHead l) of
-         True -> Just ([callHead l], t)
-         False -> Nothing
+     case isAp l of
+      True -> liftUnaryAp t l
+      False -> Nothing
+     
+liftUnaryAp t l =
+  case isAp t && (callHead t) == (callHead l) of
+   True -> 
+     let ta = callArgs t
+         la = callArgs l
+         res = sequence $ L.zipWith liftUnary ta la in
+      case res of
+       Just a ->
+         let calls = L.concatMap fst a
+             args = L.map snd a
+             h = callHead t in
+          Just (calls, ap h args)
+       Nothing -> Nothing
+   False ->
+     case (tToT $ gblType $ callHead l) && (length $ callArgs l) == 1 of
+      True -> Just ([callHead l], t)
+      False -> Nothing
 
 tToT t = isFuncType t && (arity t == 1) && (returnType t == argType 0 t)
